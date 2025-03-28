1b1582010934ac5597935b5e5c99bcd141d723e5
Wake1st <joeltwakefield@gmail.com>
2025-03-28 00:25:29 -0600

    purchased items move to bought slots

--- ui/shop/shop.gd
+++ ui/shop/shop.gd
@@ -3,23 +3,30 @@ extends Node3D
 
 
 const SLOTS_PER_ROW: int = 4
-const BUMP_TIME: float = 0.2
+const FOCUS_TIME: float = 0.2
 
 @onready var trashScene = preload("res://ui/shop/trash.tscn")
 @onready var slotsParent: Node = $ShopSlots
+@onready var boughtParent = $BoughtSlots
 
 var trashItems: Array[Trash] = []
 var shopSlots: Array[ShopSlot] = []
+var boughtSlots: Array[BoughtSlot] = []
 
 var focusedIndex: int = 0
 var focusedSlot: ShopSlot
-var bumpTimer: float = BUMP_TIME
+var focusTimer: float = FOCUS_TIME
+
+var itemCount: int
+var isSoldOut: bool = false
 
 
 func _ready():
 	# setup the slots for ui control
 	for child in slotsParent.get_children():
 		shopSlots.push_back(child)
+	for child in boughtParent.get_children():
+		boughtSlots.push_back(child)
 	
 	# fill the slots with trash
 	for slot in shopSlots:
@@ -30,33 +37,65 @@ func _ready():
 	# set initial focus
 	focusedSlot = shopSlots[0]
 	focusedSlot.focus()
+	
+	# set number of items for sale
+	itemCount = shopSlots.size()
 
 
 func _process(delta):
-	var ui_selection = UIController.get_selection()
-	if ui_selection == UIController.SELECTION.ACCEPT:
-		# do acceptance
-		pass
-	elif ui_selection == UIController.SELECTION.CANCEL:
+	var uiSelection = UIController.get_selection()
+	if uiSelection == UIController.SELECTION.CANCEL:
 		# do cancel
 		pass
 	
-	# reduce bump time
-	if bumpTimer > 0.0:
-		bumpTimer -= delta
+	# before any interactions, check the store inventory
+	if isSoldOut:
+		return
 	
-	# check focus
-	var ui_direction = UIController.get_direction()
-	if ui_direction != Vector2.ZERO and bumpTimer < 0.0:
-		# set new index
-		focusedIndex += ui_direction.x
-		focusedIndex -= ui_direction.y * SLOTS_PER_ROW
+	# attempt a trash purchase
+	if uiSelection == UIController.SELECTION.ACCEPT:
+		# TODO: validate Moni before purchase
+		
+		# move the bought item
+		var boughtSlot = boughtSlots[focusedIndex]
+		var relativePosition = focusedSlot.get_trash_position() - boughtSlot.global_position
+		boughtSlot.store_item(focusedSlot.remove_trash(), relativePosition)
+		
+		# shift to a new focus
+		refocus(Vector2(1,0), true)
+	
+	# reduce focus timer
+	if focusTimer > 0.0:
+		focusTimer -= delta
+	
+	# attempt to focus on new trash
+	var uiDirection = UIController.get_direction()
+	if uiDirection != Vector2.ZERO and focusTimer < 0.0:
+		refocus(uiDirection)
+		
+		# reset the focus timer
+		focusTimer = FOCUS_TIME
+
+
+func refocus(direction: Vector2, isPurchased: bool = false) -> void:
+	# iterate index
+	var slotsChecked: int = 0
+	while(true):
+		if slotsChecked < itemCount:
+			slotsChecked += 1
+		else:
+			isSoldOut = true
+			return
+		
+		focusedIndex += (direction.x as int)
+		focusedIndex -= (direction.y as int) * SLOTS_PER_ROW
 		focusedIndex %= shopSlots.size()
 		
-		# swap focus
+		if !shopSlots[focusedIndex].is_empty():
+			break
+	
+	# swap focus
+	if !isPurchased:
 		focusedSlot.unfocus()
-		focusedSlot = shopSlots[focusedIndex]
-		focusedSlot.focus()
-		
-		# reset the bump timer
-		bumpTimer = BUMP_TIME
+	focusedSlot = shopSlots[focusedIndex]
+	focusedSlot.focus()
--- ui/shop/shop_slot.gd
+++ ui/shop/shop_slot.gd
@@ -15,6 +15,21 @@ func add_trash(trash: Trash) -> void:
 	trash.global_position = global_position
 
 
+func remove_trash() -> Trash:
+	remove_child(item)
+	var trash:Trash = item
+	item = null
+	return trash
+
+
+func get_trash_position() -> Vector3:
+	return item.global_position
+
+
+func is_empty() -> bool:
+	return item == null
+
+
 func focus() -> void:
 	tween = create_tween()
 	tween.tween_property(item, "position:y", liftDistance, liftDuration)
