32bf456b11f02bd44871ecee1f43b4e8f764e178
Wake1st <joeltwakefield@gmail.com>
2025-03-29 19:22:42 -0600

    new tweens, who dis

--- systems/staging_system.gd
+++ systems/staging_system.gd
@@ -5,9 +5,9 @@ extends Node3D
 signal shop_opened
 signal level_opened
 
-const CAMERA_SIZE: float = 22.
-const CAMERA_FLIP: float = 3.2
-const CAMERA_SHIFT: float = 0.6
+const CAMERA_LEVEL_SIZE: float = 22.
+const CAMERA_SHIFT_SIZE: float = 0.6
+const CAMERA_SHOP_FOV: float = 40
 
 const PRE_SHIFT_TIME: float = 1.0
 const PRE_SHRINK_TIME: float = 0.8
@@ -18,19 +18,26 @@ const POST_SHIFT_TIME: float = 0.6
 @onready var levelContainer: LevelContainer = $LevelContainer
 @onready var shopContainer: ShopContainer = $ShopContainer
 
-@onready var camera: Camera3D = $Camera
+@onready var follower: PathFollow3D = %Follower
+@onready var camera: Camera3D = %Camera
 
 var cameraLevelPosition: Vector3
 var cameraOverHolePosition: Vector3
 var cameraUnderHolePosition: Vector3
 var cameraShopPosition: Vector3
 
-var tween: Tween
+var lookTween: Tween
+var sizeTween: Tween
+var transitionTime: float = PRE_SHIFT_TIME + SHIFT_TIME + POST_SHIFT_TIME
+var transitionTimer: float = 0.0
+var cameraDelay: float = transitionTime - PRE_SHRINK_TIME - POST_SHRINK_TIME
+var isGoingToShop: bool = false
+var isLeavingShop: bool = false
 
 
-func _ready():
+func _ready() -> void:
 	# ensure camera looks at origin
-	camera.size = CAMERA_SIZE
+	camera.size = CAMERA_LEVEL_SIZE
 	camera.look_at(Vector3.ZERO)
 	
 	# connect container signals
@@ -38,6 +45,14 @@ func _ready():
 	shopContainer.shop_closed.connect(handle_shop_closed)
 	levelContainer.main_selected.connect(handle_main_selected)
 	shopContainer.main_selected.connect(handle_main_selected)
+
+
+func _process(delta) -> void:
+	if isGoingToShop:
+		transitionTimer += delta
+		if transitionTimer < transitionTime:
+			var weight = lerp(0.0, transitionTime, transitionTimer)
+			follower.progress_ratio = weight
 
 
 func setup(levelNumber: int = -1) -> void:
@@ -57,23 +72,46 @@ func handle_main_selected() -> void:
 
 
 func transition_to_shop() -> void:
-	tween = create_tween()
-	# move camera over the hole
-	#tween.tween_method(camera.look_at, 0.0, 0.0, CAMERA_TO_HOLE_TIME)
-	#tween.tween_property(camera, "size", CAMERA_FLIP, SHRINK_TIME)
-	#tween.tween_property(camera, "size", CAMERA_SHIFT, SHRINK_TIME)
-	#tween.set_parallel()
-	#tween.tween_property(levelContainer, "rotation:x", PI/2 + camera.rotation.x, FLIP_TIME)
-	#tween.set_parallel(false)
-	#tween.tween_property(levelContainer, "position", camera.position * 1.4, SHIFT_TIME)
-	#tween.tween_property(camera, "size", CAMERA_SIZE, GROW_TIME)
-	#tween.tween_callback(open_shop)
+	isGoingToShop = true
+	
+	# tween where the camera looks
+	lookTween = create_tween()
+	lookTween.tween_method(camera.look_at, Vector3.ZERO, Vector3.ZERO, PRE_SHIFT_TIME)
+	lookTween.tween_method(camera.look_at, Vector3.ZERO, Vector3.DOWN * 15, SHIFT_TIME)
+	lookTween.tween_method(camera.look_at, Vector3.DOWN * 15, Vector3(0,-15,-2), POST_SHIFT_TIME)
+	
+	# tween the camera projection
+	sizeTween = create_tween()
+	sizeTween.tween_property(camera, "size", CAMERA_SHIFT_SIZE, PRE_SHRINK_TIME)
+	sizeTween.tween_interval(cameraDelay)
+	sizeTween.tween_callback(camera_flip_perspective.bind(true))
+	sizeTween.tween_property(camera, "fov", CAMERA_SHOP_FOV, POST_SHRINK_TIME)
 
 
 func transition_to_level() -> void:
-	tween = create_tween()
-	#tween.tween_property(camera, "")
-	#tween.tween_callback(run_level)
+	isLeavingShop = true
+	
+	# tween where the camera looks
+	lookTween = create_tween()
+	lookTween.tween_method(camera.look_at, Vector3(0,-15,-2), Vector3.UP * 15, POST_SHIFT_TIME)
+	lookTween.tween_method(camera.look_at, Vector3.UP * 15, Vector3.ZERO, SHIFT_TIME)
+	lookTween.tween_method(camera.look_at, Vector3.ZERO, Vector3.ZERO, PRE_SHIFT_TIME)
+	
+	# tween the camera projection
+	sizeTween = create_tween()
+	sizeTween.tween_property(camera, "fov", 1, POST_SHRINK_TIME)
+	sizeTween.tween_callback(camera_flip_perspective.bind(false))
+	sizeTween.tween_interval(cameraDelay)
+	sizeTween.tween_property(camera, "size", CAMERA_LEVEL_SIZE, PRE_SHRINK_TIME)
+
+
+func camera_flip_perspective(toPerspective: bool) -> void:
+	if toPerspective:
+		camera.projection = Camera3D.PROJECTION_PERSPECTIVE
+		camera.fov = 1.0
+	else:
+		camera.projection = Camera3D.PROJECTION_ORTHOGONAL
+		camera.size = CAMERA_SHIFT_SIZE
 
 
 func open_shop() -> void:
