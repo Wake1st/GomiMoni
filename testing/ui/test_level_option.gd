9410adeff901c09b3d6ea05e4d7dadc1559a9451
Wake1st <joeltwakefield@gmail.com>
2025-04-05 21:45:01 -0600

    added enabling to level selection

--- testing/ui/test_level_option.gd
+++ testing/ui/test_level_option.gd
@@ -3,9 +3,17 @@ extends Node3D
 
 @onready var level_option: LevelOption = $LevelOption
 
+var isEnabled: bool = false
+
 
 func _ready():
 	level_option.selected.connect(handle_option_selected)
+
+
+func _input(_event):
+	if Input.is_key_pressed(KEY_E):
+		isEnabled = !isEnabled
+		level_option.enable(isEnabled)
 
 
 func handle_option_selected(number: int) -> void:
--- ui/level selection/level_option.gd
+++ ui/level selection/level_option.gd
@@ -6,11 +6,14 @@ signal selected(number: int)
 
 const HOVER_SHIFT: float = 0.4
 const HOVER_DURATION: float = 0.1
+const MASK_COLOR: Color = Color("#3054729c")
 
 @export_range(0, 5) var number: int = 0
 @export var image: Texture2D
 
-@onready var mesh = $Mesh
+@onready var mesh: MeshInstance3D = $Mesh
+
+var isEnabled: bool = false
 
 var hasFocus: bool = false
 var basePosition: Vector3
@@ -24,6 +27,12 @@ func _ready() -> void:
 	material.albedo_texture = image
 	mesh.set_surface_override_material(0, material)
 	
+	# set up a disabled mask
+	#var mask: StandardMaterial3D = StandardMaterial3D.new()
+	#mask.albedo_color = MASK_COLOR
+	#mask.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	#mesh.material_override = mask
+	
 	# set animation values
 	basePosition = position
 	hoverPosition = position + basis.z * HOVER_SHIFT
@@ -33,8 +42,20 @@ func setup(camera_position: Vector3) -> void:
 	look_at(camera_position)
 
 
+func enable(value: bool) -> void:
+	isEnabled = value
+	
+	#var mask: StandardMaterial3D = mesh.material_override
+	#if isEnabled:
+		## set a mask on
+		#mask.transparency = BaseMaterial3D.TRANSPARENCY_DISABLED
+	#else:
+		#mask.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	#mesh.material_override = mask
+
+
 func _input(event) -> void:
-	if event.is_action_pressed("ui_accept") && hasFocus:
+	if isEnabled && event.is_action_pressed("ui_accept") && hasFocus:
 		emit_signal("selected", number)
 		
 		# animate selection
@@ -44,18 +65,22 @@ func _input(event) -> void:
 
 
 func _on_mouse_entered():
-	hasFocus = true
-	
-	# animate toward camera
-	tween = create_tween()
-	tween.tween_property(self, "position", hoverPosition, HOVER_DURATION)
-	tween.set_ease(Tween.EASE_OUT)
+	# ensure level is clickable
+	if isEnabled:
+		hasFocus = true
+		
+		# animate toward camera
+		tween = create_tween()
+		tween.tween_property(self, "position", hoverPosition, HOVER_DURATION)
+		tween.set_ease(Tween.EASE_OUT)
 
 
 func _on_mouse_exited():
-	hasFocus = false
-	
-	# animate away from camera
-	tween = create_tween()
-	tween.tween_property(self, "position", basePosition, HOVER_DURATION)
-	tween.set_ease(Tween.EASE_OUT)
+	# ensure level is clickable
+	if isEnabled:
+		hasFocus = false
+		
+		# animate away from camera
+		tween = create_tween()
+		tween.tween_property(self, "position", basePosition, HOVER_DURATION)
+		tween.set_ease(Tween.EASE_OUT)
