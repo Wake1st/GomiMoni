1dacda067a70daf2b9ffaa0af699c8e13b1dbf8e
Wake1st <joeltwakefield@gmail.com>
2025-04-06 20:56:42 -0600

    testing updates

---
+++ testing/stages/test_level_container.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+
+@export_range(0,5) var testingLevel: int
+
+@onready var levelContainer: LevelContainer = $LevelContainer
+
+
+func _ready():
+	levelContainer.level_ready.connect(handle_level_ready)
+	levelContainer.level_closed.connect(handle_level_closed)
+	
+	levelContainer.setup(testingLevel)
+
+
+func _input(_event):
+	if Input.is_action_just_pressed("ui_accept"):
+		levelContainer.level.reset()
+
+
+func handle_level_ready() -> void:
+	levelContainer.open()
+
+
+func handle_level_closed() -> void:
+	levelContainer.teardown()
+	levelContainer.setup(testingLevel)
